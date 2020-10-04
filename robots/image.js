671867eb14cfe3e202871437b0fdc099337823bb
Matheusdk75 <matheus.cbmdk@gmail.com>
2020-10-04 15:56:45 -0300

    feat: add image download logic witi fallback

--- robots/image.js
+++ robots/image.js
@@ -1,3 +1,4 @@
+const imageDownloader = require('image-downloader');
 const google = require('googleapis').google;
 const customSearch = google.customsearch('v1');
 const state = require('./state');
@@ -6,9 +7,11 @@ const googleSearchCredentials = require('../credentials/google-search.json');
 
 async function robot() {
   const content = state.load();
+
   await fetchImageOfAllSentences(content);
-  console.dir(content, { depth: null });
-  process.exit(0);
+  await downloadAllImages(content);
+
+  state.save(content);
 
   async function fetchImageOfAllSentences(content) {
     for (const sentence of content.sentences) {
@@ -34,6 +37,48 @@ async function robot() {
     });
 
     return imagesUrl;
+  }
+
+  async function downloadAllImages(content) {
+    content.downloadedImages = [];
+
+    for (
+      let sentenceIndex = 0;
+      sentenceIndex < content.sentences.length;
+      sentenceIndex++
+    ) {
+      const images = content.sentences[sentenceIndex].images;
+
+      for (let imageIndex = 0; imageIndex < images.length; imageIndex++) {
+        const imageUrl = images[imageIndex];
+
+        try {
+          if (content.downloadedImages.includes(imageUrl)) {
+            throw new Error('Imagem já foi baixada');
+          }
+
+          await downloadAndSave(imageUrl, `${sentenceIndex}-original.png`);
+          content.downloadedImages.push(imageUrl);
+          console.log(
+            `>[${sentenceIndex}][${imageIndex}] Baixou imagem com sucesso: ${imageUrl}`
+          );
+          break;
+        } catch (error) {
+          console.log(
+            `>[${sentenceIndex}][${imageIndex}] Erro ao baixar (${imageUrl}): ${error}`
+          );
+        }
+      }
+    }
+  }
+
+  async function downloadAndSave(url, fileName) {
+    // ele vai salvar na pasta raiz -> pasta content (já precisa estar criada)
+    return imageDownloader.image({
+      url,
+      url,
+      dest: `./content/${fileName}`,
+    });
   }
 }
 
